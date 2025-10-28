b314fc6ec222e03ab57415a54462fc4436e76161
DGoblot <david.goblot26@gmail.com>
2025-10-28 15:47:08 +0100

    sound test

--- scripts/click_button_poussoire.gd
+++ scripts/click_button_poussoire.gd
@@ -1,9 +1,11 @@
 extends StaticBody3D
 
 @onready var anim = $"../AnimationPlayer"
+@onready var fmod = $"../../FmodEventEmitter3D"
 
 func _input_event(camera, event, click_position, click_normal, shape_idx):
 	if event is InputEventMouseButton:
 		print("Mouse click/unclicked at: ", event.position, " shape:", shape_idx)
 		if !anim.is_playing():
 			anim.play("Push Poussoire 1")
+			fmod.play()
