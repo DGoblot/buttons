295f253dac7b32c9283afa18447e83b436efc9f4
Yvann <yvann.nadal@gmail.com>
2025-10-29 14:38:47 +0100

    fix leaderboard

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,15 +1,28 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-  SilentWolf.configure({
-	"api_key": "6HfJmFfH0w4mm8FDS9SKP69cf4OytO0H2VgMZ8Uy",
-	"game_id": "pushit",
-	"log_level": 1
-  })
+	SilentWolf.configure({
+		"api_key": "6HfJmFfH0w4mm8FDS9SKP69cf4OytO0H2VgMZ8Uy",
+		"game_id": "pushit",
+		"log_level": 1
+	})
 
-  SilentWolf.configure_scores({
-	"open_scene_on_close": "res://scenes/main.tscn"
-  })
+	SilentWolf.configure_scores({
+		"open_scene_on_close": "res://scenes/main.tscn"
+	})
+
+	if has_node("LoadingLabel"):
+		$LoadingLabel.text = "Chargement du classement..."
+		$LoadingLabel.visible = true
+
+	SilentWolf.Scores.connect("scores_received", Callable(self, "_on_scores_received"))
+
+	SilentWolf.Scores.get_scores(10)
+
+func _on_scores_received(scores):
+	if has_node("LoadingLabel"):
+		$LoadingLabel.visible = false
+
+	for i in range(len(scores)):
+		var s = scores[i]
+		print("%d. %s - %d" % [i + 1, s["player_name"], s["score"]])
