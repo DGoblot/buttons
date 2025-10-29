7eb7b4172242c52b00c0fdf3d16e83233270011b
Naessala <poirepine@gmail.com>
2025-10-29 10:52:45 +0100

    feat: fix volume states

--- scripts/click_button_flipflop2.gd
+++ scripts/click_button_flipflop2.gd
@@ -1,15 +1,22 @@
 extends StaticBody3D
 
 @onready var anim = $"../AnimationPlayer"
+@onready var tick = $"../Tick"
+@onready var pitchPlus = $"../PitchPlus"
+
 var state = "on"
 
 func _input_event(camera, event, click_position, click_normal, shape_idx):
 	if event is InputEventMouseButton:
 		print("Mouse click/unclicked at: ", event.position, " shape:", shape_idx)
 		if !anim.is_playing():
+			tick.play()
+			
 			if state == "off":
 				anim.play("Flip2")
 				state = "on"
+				pitchPlus.play()
 			else:
 				anim.play("Flop2")
 				state = "off"
+				pitchPlus.play()
