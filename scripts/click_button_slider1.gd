519e73558e2c160400853fc6344f50e04f99657f
DGoblot <david.goblot26@gmail.com>
2025-10-29 11:09:39 +0100

    volume slider

--- scripts/click_button_slider1.gd
+++ scripts/click_button_slider1.gd
@@ -1,19 +1,53 @@
 extends StaticBody3D
 
 @onready var anim = $"../AnimationPlayer"
-@onready var slider = $"../../FmodEventEmitter3D"
-var state = "off"
+@onready var slider = $"../../Slide"
+@onready var vol0_1 = $"../../Volume 0_1"
+@onready var vol1_2 = $"../../Volume 1_2"
+@onready var vol2_3 = $"../../Volume 2_3"
+@onready var vol3_4 = $"../../Volume 3_4"
+@onready var vol4_5 = $"../../Volume 4_5"
+@onready var vol5_0 = $"../../Volume 5_0"
+
+var state = 2
+var animLength
+
+func _ready() -> void:
+	anim.speed_scale = 0.8
+	vol1_2.play()
 
 func _input_event(camera, event, click_position, click_normal, shape_idx):
 	if event is InputEventMouseButton:
 		print("Mouse click/unclicked at: ", event.position, " shape:", shape_idx)
 		if !anim.is_playing():
-			anim.speed_scale = 0.8
-			if state == "off":
-				anim.play("Slider 1 Off")
-				state = "on"
-				slider.play()
-			else:
-				anim.play("Slider 1 On")
-				state = "off"
-				slider.play()
+			match state:
+				0:
+					anim.play("Slider 1 0_1")
+					state = 1
+					slider.play()
+					vol0_1.play()
+				1:
+					anim.play("Slider 1 1_2")
+					state = 2
+					slider.play()
+					vol1_2.play()
+				2:
+					anim.play("Slider 1 2_3")
+					state = 3
+					slider.play()
+					vol2_3.play()
+				3:
+					anim.play("Slider 1 3_4")
+					state = 4
+					slider.play()
+					vol3_4.play()
+				4:
+					anim.play("Slider 1 4_5")
+					state = 5
+					slider.play()
+					vol4_5.play()
+				5:
+					anim.play("Slider 1 5_0")
+					state = 0
+					slider.play()
+					vol5_0.play()
