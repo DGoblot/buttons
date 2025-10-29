d6746e649939c585770b0bd037be53e6228095dc
Naessala <poirepine@gmail.com>
2025-10-29 10:15:01 +0100

    feat: filter sliders

--- scripts/click_button_slider2.gd
+++ scripts/click_button_slider2.gd
@@ -1,18 +1,25 @@
 extends StaticBody3D
 
 @onready var anim = $"../AnimationPlayer"
+@onready var sonSliderB = $"../SonSlider"
+@onready var lowPassOn = $"../FilterOn"
+@onready var lowPassOff = $"../FilterOff"
 var state = "on"
 
 func _input_event(camera, event, click_position, click_normal, shape_idx):
 	if event is InputEventMouseButton:
 		print("Mouse click/unclicked at: ", event.position, " shape:", shape_idx)
+		sonSliderB.play()
+		fmod
 		if !anim.is_playing():
 			if state == "off":
 				anim.play("Slider 2 Off")
 				state = "on"
+				lowPassOff.play()
 			else:
 				anim.play("Slider 2 On")
 				state = "off"
+				lowPassOn.play()
 
 func _process(delta: float) -> void:
 	if anim.is_playing():
